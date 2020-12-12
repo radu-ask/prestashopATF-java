3b2626df517b8545aecf69cb28925df787012ccf
radu2020 <radu.ask@gmail.com>
2020-12-12 18:54:15 +0200

    [09] add components/NavigationMenu.java and components/TopHeader.java

---
+++ prestashop-domain/src/main/java/com/prestashop/domain/pages/components/NavigationMenu.java
@@ -0,0 +1,36 @@
+package com.prestashop.domain.pages.components;
+
+import com.prestashop.domain.pages.AbstractBasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NavigationMenu extends AbstractBasePage {
+
+    @FindBy(xpath = "//*[@id='_desktop_logo']/a")
+    private WebElement logo;
+
+    @FindBy(xpath = "//*[@id='category-3']/a")
+    private WebElement menuItemClothes;
+
+    @FindBy(xpath = "//*[@id='category-6']/a")
+    private WebElement menuItemAccessories;
+
+    @FindBy(xpath = "//*[@id='category-9']/a")
+    private WebElement menuItemArt;
+
+    @FindBy(xpath = "//*[@placeholder='Search our catalog']")
+    private WebElement textboxSearch;
+
+    @FindBy(xpath = "//*[@id='search_widget']/form/button")
+    private WebElement buttonSearch;
+
+    public NavigationMenu(WebDriver driver) {
+        super(driver);
+    }
+
+    public void searchFor(String item) {
+        setElementText(textboxSearch, item);
+        buttonSearch.click();
+    }
+}
---
+++ prestashop-domain/src/main/java/com/prestashop/domain/pages/components/TopHeader.java
@@ -0,0 +1,29 @@
+package com.prestashop.domain.pages.components;
+
+import com.prestashop.domain.pages.AbstractBasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TopHeader extends AbstractBasePage {
+
+    @FindBy(xpath = "//*[@id='contact-link']/a")
+    private WebElement linkContactUs;
+
+    @FindBy(xpath = "//*[@aria-label='Language dropdown']")
+    private WebElement dropdownLanguage;
+
+    @FindBy(xpath = "//*[@title='Log in to your customer account']")
+    private WebElement linkSignIn;
+
+    @FindBy(xpath = "//*[@id='_desktop_cart']//child::span[contains(text(), 'Cart')]")
+    private WebElement cart;
+
+    public TopHeader(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickContactUsLink(){
+        linkContactUs.click();
+    }
+}
